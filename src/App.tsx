2f43eb8f873b5f721d144d0a2ed315f538d1f9b7
erobert <etiennerobert33@gmail.com>
2021-09-23 23:45:42 +0200

    Basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import { useState } from 'react';
+
 import './App.css';
 
+type TaskId = string;
+
+type Task = {
+  id: TaskId;
+  name: string;
+};
+
+const mockTask1 = {
+  id: '1',
+  name: 'task-1',
+};
+
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([mockTask1]);
+
   return (
     <div className="App">
-      Hello World!
-
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.name}</li>
+        ))}
+      </ul>
+      {tasks.length === 0 ? "You're done!" : 'Next: ' + tasks[0].name}
     </div>
   );
 }
