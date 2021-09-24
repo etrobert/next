91c6aa4d37838138708a678e3a16b149082b8253
erobert <etiennerobert33@gmail.com>
2021-09-24 16:44:16 +0200

    Add new task form

--- src/ListView/ListView.tsx
+++ src/ListView/ListView.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 
 import { projectState } from 'atoms';
@@ -8,15 +9,43 @@ import './ListView.css';
 
 const ListView = () => {
   const { tasks } = useRecoilValue(projectState);
+  const [newTaskName, setNewTaskName] = useState<string>('');
 
   return (
-    <table className="ListView">
-      <tbody>
-        {tasks.map((task) => (
-          <Task key={task} id={task} />
-        ))}
-      </tbody>
-    </table>
+    <>
+      <form
+        id="new-task-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(newTaskName);
+        }}
+      />
+      <table className="ListView">
+        <tbody>
+          {tasks.map((task) => (
+            <Task key={task} id={task} />
+          ))}
+          <tr>
+            <td>
+              <button
+                className="ListView__new-task-submit"
+                type="submit"
+                form="new-task-form"
+              />
+            </td>
+            <td>
+              <input
+                className="ListView__new-task-input"
+                type="text"
+                form="new-task-form"
+                value={newTaskName}
+                onChange={(e) => setNewTaskName(e.target.value)}
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
   );
 };
 
