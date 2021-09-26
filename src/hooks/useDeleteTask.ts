4f138e11f2de76e25a5cad145c6a9a6c32b805fd
erobert <etiennerobert33@gmail.com>
2021-09-27 00:03:11 +0200

    Update useDeleteTask to delete related dependencies

--- src/hooks/useDeleteTask.ts
+++ src/hooks/useDeleteTask.ts
@@ -1,6 +1,15 @@
 import { useCallback } from 'react';
 import { useRecoilValue } from 'recoil';
-import { deleteDoc, doc } from 'firebase/firestore';
+import {
+  collection,
+  doc,
+  DocumentData,
+  getDocs,
+  query,
+  QueryDocumentSnapshot,
+  where,
+  writeBatch,
+} from 'firebase/firestore';
 
 import { projectIdState } from 'atoms';
 import { TaskId } from 'types';
@@ -10,8 +19,36 @@ const useDeleteTask = () => {
   const projectId = useRecoilValue(projectIdState);
 
   return useCallback(
-    (id: TaskId) =>
-      deleteDoc(doc(firestore, `projects/${projectId}/tasks/${id}`)),
+    async (id: TaskId) => {
+      const batch = writeBatch(firestore);
+      const depsRef = collection(
+        firestore,
+        `projects/${projectId}/dependencies`
+      );
+      const predsQuery = query(depsRef, where('successor', '==', id));
+      const succsQuery = query(depsRef, where('predecessor', '==', id));
+
+      const [preds, succs] = await Promise.all([
+        getDocs(predsQuery),
+        getDocs(succsQuery),
+      ]);
+
+      const deleteFromSnapshot = (
+        snapshot: QueryDocumentSnapshot<DocumentData>
+      ) => {
+        const ref = doc(
+          firestore,
+          `projects/${projectId}/dependencies/${snapshot.id}`
+        );
+        batch.delete(ref);
+      };
+
+      preds.forEach(deleteFromSnapshot);
+      succs.forEach(deleteFromSnapshot);
+
+      batch.delete(doc(firestore, `projects/${projectId}/tasks/${id}`));
+      return batch.commit();
+    },
     [projectId]
   );
 };
